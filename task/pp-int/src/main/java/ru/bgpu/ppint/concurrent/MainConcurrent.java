09bfc0396b663fd4b27f13e89dc11a0b14188071
goldfinch_p8a <wanted1345@gmail.com>
2021-10-29 12:48:00 +0900

    add sorting results

--- task/pp-int/src/main/java/ru/bgpu/ppint/concurrent/MainConcurrent.java
+++ task/pp-int/src/main/java/ru/bgpu/ppint/concurrent/MainConcurrent.java
@@ -2,20 +2,40 @@ package ru.bgpu.ppint.concurrent;
 
 import ru.bgpu.ppint.Function;
 import ru.bgpu.ppint.Integral;
+import ru.bgpu.ppint.IntegralThread;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class MainConcurrent {
+
+    private static Map<Double,  List<Future<Double>>> results = new TreeMap<>();
+
     public static void main(String[] args) {
         Double a = 0D, b = Math.PI, width = Math.pow(10,-8);
         Function fun = Math::sin;
         for (int i = 1; i <= 10; i++) {
             createIntegralThreads(i, a, b, width, fun);
+        }
+        System.out.println("\nResults:");
+        for (Map.Entry<Double, List<Future<Double>>> entry : results.entrySet()) {
+            int threadCount = entry.getValue().size();
+            Double res = entry.getValue().stream().mapToDouble(future -> {
+                try {
+                    return future.get();
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+                return 0;
+            }).sum();
+            Double timeExec = entry.getKey();
+            System.out.printf("Потоки [%2d] : %.4f - %.3f%n", threadCount, res,timeExec);
         }
     }
 
@@ -37,14 +57,7 @@ public class MainConcurrent {
             e.printStackTrace();
         }
         executorService.shutdown();
-        Double result = futures.stream().mapToDouble(f -> {
-            try {
-                return f.get();
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
-            }
-            return 0;
-        }).sum();
-        System.out.printf("Потоки [%d] : %.4f - %.3f%n",threadsCount, result, (System.currentTimeMillis()-time)/1000D);
+        results.put((System.currentTimeMillis()-time)/1000D, futures);
+        System.out.println("Working " + threadsCount + " threads complete");
     }
 }
